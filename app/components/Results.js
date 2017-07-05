9807d9e6236082ef283548a2f4649cec184dfb20
atiquzzaman_soikat <atik.aiub@gmail.com>
2017-07-05 23:19:20 +0600

    added all codes for github battle

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,116 @@
-var React = require('react');
+var React = require("react");
+var queryString = require("query-string");
+var api = require("../utils/api.js");
+var Link = require('react-router-dom').Link;
+var PropTypes = require('prop-types');
+var PlayerPreview = require('./PlayerPreview');
+const Loading = require('./Loading');
 
-class Results extends React.Component{
-    render(){
+function Profile(props){
+    var info = props.info;
+    return(
+        <PlayerPreview avatar={info.avatar_url} username={info.login} >
+        <ul className='space-list-items'>
+            {info.name && <li>{info.name}</li>}
+            {info.location && <li>{info.location}</li>}
+            {info.company && <li>{info.company}</li>}
+            <li>Followers: {info.followers}</li>
+            <li>Following: {info.following}</li>
+            <li>Public Repos: {info.public_repos}</li>
+            {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+        </ul>
+        </PlayerPreview>    
+    )
+}
+
+Profile.PropTypes = {
+    info: PropTypes.object.isRequired
+}
+
+function Player(props){
+    return(
+        <div>
+            <h1 className="header">{props.label}</h1>
+            <h3 style={{textAlign: 'center'}}>{props.score}</h3>
+            <Profile info={props.profile} />
+        </div>    
+    );
+}
+
+
+Player.PropTypes = {
+    label: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired,
+    profile: PropTypes.object.isRequired
+}
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    };
+  }
+  componentDidMount() {
+    var players = queryString.parse(this.props.location.search);
+
+    api
+      .battle([players.playerOneName, players.playerTwoName])
+      .then(function(result) {
+        if (result === null) {
+          this.setState(function() {
+            return {
+              error: "Looks like there was an error.",
+              loading: false
+            };
+          });
+        }
+
+        this.setState(function() {
+          return {
+            error: null,
+            winner: result[0],
+            loser: result[1],
+            loading: false
+          };
+        });
+      }.bind(this));
+  }
+  render() {
+    var error = this.state.error;
+    var winner = this.state.winner;
+    var loser = this.state.loser;
+    var loading = this.state.loading;
+    if (loading) {
+      return <Loading />;
+    }
+    if(error){
         return(
             <div>
-               <p>Result will be show here</p> 
+              {error}
+              <Link to="/battle">Reset</Link>  
             </div>    
         )
     }
+    return (
+      <div className="row">
+        <Player
+            label="Winner"
+            score={winner.score}
+            profile={winner.profile}    
+         />
+        <Player
+            label="Loser"
+            score={loser.score}
+            profile={loser.profile}    
+         />
+      </div>
+    );
+  }
 }
 
 module.exports = Results;
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,59 @@
 const axios = require("axios");
 
+const id = "xxx";
+const sec = "xxx";
+const params = `?client_id=${id}&client_secret=${sec}`;
+
+function getProfile(username) {
+  return axios
+    .get(`https://api.github.com/users/${username}`)
+    .then(function(user) {
+      return user.data;
+    });
+}
+function getRepos(username) {
+  return axios.get(
+    `https://api.github.com/users/${username}/repos?per_page=100`
+  );
+}
+function getStarCount(repos) {
+  return repos.data.reduce(function(count, repo) {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+function calculateScore(profile, repos) {
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos);
+  debugger;
+  return followers * 3 + totalStars;
+}
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+function getUserData(player) {
+  return axios.all([getProfile(player), getRepos(player)]).then(function(data) {
+    var profile = data[0];
+    var repos = data[1];
+    console.log(repos);
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    };
+  });
+}
+function sortPlayers(players) {
+  return players.sort(function(a, b) {
+    return b.score - a.score;
+  });
+}
 module.exports = {
+  battle: function(players) {
+    return axios
+      .all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError);
+  },
   fetchPopularRepos(language) {
     const encodedURI = window.encodeURI(
       "https://api.github.com/search/repositories?q=stars:>1+language:" +
